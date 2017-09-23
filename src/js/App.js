0a95bae0f06ecee554a6daf0e1f11874242e099e
Anett_Soos <anett.soos@city.ac.uk>
2017-09-23 17:14:20 +0100

    item-list

---
+++ src/js/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ItemList from './item-list'
+
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+		  <ItemList/>
+	  </div>
+   )
+  }
+}
+
+export default App;
