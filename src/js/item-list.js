c31701ac871b5a12cb8757543d578069aa6f14bf
Anett_Soos <anett.soos@city.ac.uk>
2017-09-25 23:18:48 +0100

    Link

--- src/js/item-list.js
+++ src/js/item-list.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import ReactDOM from 'react-dom' 
 import Redux from 'redux' 
-import { createStore } from 'redux' 
+import { createStore } from 'redux'
+import { Link } from 'react-router-dom'
  
 class ItemList extends React.Component { 
   constructor() { 
@@ -17,8 +17,12 @@ class ItemList extends React.Component {
     return( 
       <ul> 
           {this.state.items.length ? 
-            this.state.items.map(item=><li key={item.id}>{item.title} <img src={item.thumbnailUrl} /></li>)  
-            : <li>Loading...</li> 
+            this.state.items.map(item=>
+              <li key={item.id}>
+                {item.id} 
+                <img src={item.thumbnailUrl} />
+                <Link to={`/${item.id}`}> Profile </Link> 
+              </li>) : <li>Loading...</li> 
           } 
       </ul> 
    ) 
