150d0cbca9a116ace9fd1f9bad6403749770b8af
Anett_Soos <anett.soos@city.ac.uk>
2017-09-25 10:52:57 +0100

    item-list item fix previous commit error

--- src/js/item-list.js
+++ src/js/item-list.js
@@ -1,5 +1,28 @@
 import React from 'react'
-
-const ItemList = () => <div></div>
+import ReactDOM from 'react-dom' 
+import Redux from 'redux' 
+import { createStore } from 'redux' 
+ 
+class ItemList extends React.Component { 
+  constructor() { 
+    super(); 
+     this.state={items:[]}; 
+  } 
+  componentDidMount(){ 
+    fetch(`http://jsonplaceholder.typicode.com/photos`) 
+    .then(result=>result.json()) 
+    .then(items=>this.setState({items})) 
+  } 
+  render() { 
+    return( 
+      <ul> 
+          {this.state.items.length ? 
+            this.state.items.map(item=><li key={item.id}>{item.title} <img src={item.thumbnailUrl} /></li>)  
+            : <li>Loading...</li> 
+          } 
+      </ul> 
+   ) 
+  } 
+}
 
 export default ItemList;
--- src/js/item.js
+++ src/js/item.js
@@ -1,28 +1,5 @@
 import React from 'react'
-import Redux from 'redux'
-import { createStore } from 'redux'
-import { Link } from 'react-router-dom'
 
-class Item extends React.Component {
-  constructor() {
-    super();
-     this.state={items:[]};
-  }
-  componentDidMount(){
-    fetch(`http://jsonplaceholder.typicode.com/photos`)
-    .then(result=>result.json())
-    .then(items=>this.setState({items}))
-  }
-  render() {
-    return(
-      <div>
-          {this.state.items.length ?
-            this.state.items.map(item=><li key={item.id}>{item.title} <img src={item.thumbnailUrl} /></li>) 
-            : <li>Loading...</li>
-          }
-      </div>
-   )
-  }
-}
+const Item = () => <div></div> 
 
 export default Item;
