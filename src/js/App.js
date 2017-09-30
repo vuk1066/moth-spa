49045d0c004f832efa98a7230aeb678463741e96
Anett_Soos <anett.soos@city.ac.uk>
2017-09-30 16:31:20 +0100

    data loading to new component

--- src/js/App.js
+++ src/js/App.js
@@ -1,32 +1,31 @@
-import React from 'react'
-import { BrowserRouter, Switch, Route }  from 'react-router-dom'
-import ItemList from './item-list'
-import Item from './item'
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import ItemList from "./item-list";
+import Item from "./item";
+import imagesData from "./data";
 
-class App extends React.Component { 
-  constructor(props) { 
-    super(props); 
-      this.state={
-         items:[]}; 
-      }
-  componentDidMount(){ 
-    fetch(`http://jsonplaceholder.typicode.com/photos`)  
-    .then(result=>result.json())  
-    .then(items=>this.setState({items}))  
-  } 
-  render() { 
-    return( 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    };
+  }
+  componentDidMount() {
+    imagesData().then(items => this.setState({ items }));
+  }
+  render() {
+    return (
       <BrowserRouter>
         <div>
           <Switch>
             <Route exact path="/" render={() => <ItemList result={this.state.items} />}/>
-            <Route exact path="/:pictureId" render={() => 
-              <Item image={this.state.items}/>}/>
+            <Route path="/:pictureId" component={Item} />
           </Switch>
         </div>
       </BrowserRouter>
-   ) 
-  } 
-} 
+    );
+  }
+}
 
 export default App;
